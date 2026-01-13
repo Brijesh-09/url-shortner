50f9a422c0a26e964d95b1382702a40cf59e498f
Brijesh-09 <brijeshkori22@gmail.com>
2026-01-13 22:44:39 +0530

    logging middleware add

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/Brijesh-09/internal/handlers"
 	"github.com/Brijesh-09/storage"
 )
 
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		log.Printf("[LOG] Started %s %s", r.Method, r.URL.Path)
+
+		next(w, r)
+
+		duration := time.Since(start)
+		log.Printf("[LOG] Completed in %v", duration)
+	}
+}
 func main() {
 
 	// Initialize storage
@@ -18,9 +30,9 @@ func main() {
 
 	// Setup routes
 	mux := http.NewServeMux()
-	mux.HandleFunc("/health", handler.Health)
-	mux.HandleFunc("/create", handler.Create)
-	mux.HandleFunc("/", handler.Redirect)
+	mux.HandleFunc("/health", Logger(handler.Health))
+	mux.HandleFunc("/create", Logger(handler.Create))
+	mux.HandleFunc("/", Logger(handler.Redirect))
 
 	// Start server
 	log.Println("Starting server on :9000")
